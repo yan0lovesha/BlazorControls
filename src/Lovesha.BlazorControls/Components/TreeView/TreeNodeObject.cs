6a4fd873ba25d481c66ea6a4e1482d28b18253a4
Ling Yan <neilyan@microsoft.com>
2019-06-28 16:35:09 -0700

    Adjusted the project structure.

--- src/Lovesha.BlazorControls/Components/TreeView/TreeNodeObject.cs
+++ src/Lovesha.BlazorControls/Components/TreeView/TreeNodeObject.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Lovesha.BlazorControls.TreeView
+namespace Lovesha.BlazorControls.Components.TreeView
 {
     public class TreeNodeObject
     {
