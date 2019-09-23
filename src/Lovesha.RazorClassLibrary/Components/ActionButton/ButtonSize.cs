1e794f5d88a919721fbbd77e497ba1b61ec26278
Ling Yan <neilyan@microsoft.com>
2019-09-23 12:17:26 -0700

    Create new project for cotnetcore 3.0

---
+++ src/Lovesha.RazorClassLibrary/Components/ActionButton/ButtonSize.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lovesha.RazorClassLibrary.Components.ActionButton
+{
+    public enum ButtonSize
+    {
+        Large,
+        Small
+    }
+}
