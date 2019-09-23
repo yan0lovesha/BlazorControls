d1fd1d868c11496c13228c924233c739e9d8d0e3
Ling Yan <neilyan@microsoft.com>
2019-09-23 14:11:44 -0700

    Add tree view control.

---
+++ src/Lovesha.RazorClassLibrary/Components/TreeView/TreeNodeObject.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lovesha.RazorClassLibrary.Components.TreeView
+{
+    public class TreeNodeObject
+    {
+        public string Text { get; set; }
+        public JToken NodeData { get; set; }
+        public bool AlwaysReloadChildren { get; set; }
+        public bool IsExpanded { get; set; }
+
+        public List<TreeNodeObject> ChildNodes { get; set; } = new List<TreeNodeObject>();
+
+        public Func<TreeNodeObject, Task<List<TreeNodeObject>>> LoadChildNodesFunc { get; set; }
+    }
+}
